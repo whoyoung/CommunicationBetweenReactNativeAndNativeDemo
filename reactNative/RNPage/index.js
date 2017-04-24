a6bc55ea16dc261b31bacfec6bc9a6227f0f881b
杨虎 <yanghu@xiaoshouyi.com>
2017-04-24 09:40:26 +0800

    add back button

--- reactNative/RNPage/index.js
+++ reactNative/RNPage/index.js
@@ -2,15 +2,21 @@ import React,{Component} from 'react';
 import {
   View,
   Text,
+  InteractionManager,
 } from 'react-native';
-
+import YHNativeInvokeModule from '../Native/NativeInvokeModule';
 class Page extends Component{
-    
+  _popCurrentPage() {
+    InteractionManager.runAfterInteractions(() => {
+            YHNativeInvokeModule.popCurrentPage()
+        });
+  }
   render(){
     let title = this.props.from ? this.props.from : 'RNPage';
     return(
       <View style ={{flex:1,alignItems:'center',justifyContent:'center'}}>
           <Text style ={{fontSize:18}}>{title}</Text>
+          <Text style ={{fontSize:18,marginTop:10,backgroundColor:'#26b8f2'}} onPress ={()=>this._popCurrentPage()}>previousPage</Text>
       </View>
     )
   }
